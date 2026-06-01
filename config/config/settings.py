c048d38375cf7fc6dd0deda480316a2b126f6a4e
Hicham Ouahbi <wan1hao@qq.com>
2026-06-01 12:03:04 +0800

    -

--- config/config/settings.py
+++ config/config/settings.py
@@ -68,14 +68,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+import pymysql
+pymysql.version_info = (2, 2, 1, "final", 0)  # satisfy Django's check
+pymysql.install_as_MySQLdb()
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_db',
+        'USER': 'blog_user',
+        'PASSWORD': 'blog_pass',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
